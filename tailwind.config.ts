87a7b2cdf09a6c15fd38a70ee3bc3a24a6a1a7fc
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-04-03 16:21:42 +0800

    Import Inter var font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,21 @@
 import files from './files'
 import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
   content: [...files],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          ['Inter var', ...fontFamily.sans],
+          {
+            fontFeatureSettings: '"cv11", "ss01"',
+            fontVariationSettings: '"opsz" 32',
+          },
+        ],
+      },
+    },
   },
   plugins: [],
 } satisfies Config
