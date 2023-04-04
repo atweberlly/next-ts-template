37988482f7c17ac6d664b93bfd734e6f2eb6bb32
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-04-04 08:13:01 +0800

    Import colors and font sizes from Untitled UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,17 @@
 import files from './files'
+import { colors, fontSizes } from './untitled-ui'
 import type { Config } from 'tailwindcss'
 import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
   content: [...files],
   theme: {
+    colors: {
+      ...colors,
+    },
+    fontSize: {
+      ...fontSizes,
+    },
     extend: {
       fontFamily: {
         sans: [
@@ -14,6 +21,9 @@ export default {
             fontVariationSettings: '"opsz" 32',
           },
         ],
+      },
+      minWidth: {
+        xs: '320px',
       },
     },
   },
