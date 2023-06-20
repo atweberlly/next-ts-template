8c55cbd7ff1dc7750b90f97bc48cd58f670ff355
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-06-20 17:10:45 +0800

    Fix order

--- untitled-ui/fontSizes.ts
+++ untitled-ui/fontSizes.ts
@@ -5,10 +5,10 @@ const fontSizes = {
   lg: ['1.125rem', { lineHeight: '1.75rem' }],
   xl: ['1.25rem', { lineHeight: '1.875rem' }],
   'display-xs': ['1.5rem', { lineHeight: '2rem' }],
-  'display-xl': ['3.75rem', { lineHeight: '4.5rem' }],
   'display-sm': ['1.875rem', { lineHeight: '2.375rem' }],
   'display-md': ['2.25rem', { lineHeight: '2.75rem' }],
   'display-lg': ['3rem', { lineHeight: '3.75rem' }],
+  'display-xl': ['3.75rem', { lineHeight: '4.5rem' }],
   'display-2xl': ['4.5rem', { lineHeight: '5.625rem' }],
 } satisfies Record<string, [string, { lineHeight: string }]>
 
