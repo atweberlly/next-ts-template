7dcf95bc74e02ce9bc18cf666f3dc6bcd562ed10
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-12-06 20:10:15 +0800

    Remove localFont import and related code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,6 @@
 import type { AppProps } from 'next/app'
-import localFont from 'next/font/local'
 import Head from 'next/head'
 import '~/styles/main.css'
-
-const inter = localFont({
-  src: [
-    {
-      path: '../../public/fonts/Inter-roman.var.woff2',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/Inter-italic.var.woff2',
-      style: 'italic',
-    },
-  ],
-  weight: '100 900',
-  variable: '--font-inter',
-})
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
@@ -24,11 +8,6 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <style>{`
-          :root {
-            --font-inter: ${inter.variable};
-          }
-        `}</style>
       </Head>
 
       <Component {...pageProps} />
